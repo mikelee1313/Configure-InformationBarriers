1405af830aee2d1538d68a297b3de310ffccd7d1
Mike Lee <62190454+mikelee1313@users.noreply.github.com>
2024-09-19 16:22:19 -0400

    Update Configure-InformationBarriers.ps1

--- Configure-InformationBarriers.ps1
+++ Configure-InformationBarriers.ps1
@@ -75,11 +75,16 @@ Connect-SPOService -Url ('https://' + $t + '-admin.sharepoint.com')
 
 
 #Create an Address Book Policy for all Mailboxes to prevent Empty Address Book
-Write-Host "Adding role 'Address Lists' to 'Organization Management' to allow for Address Book Management with the GA Account" -ForegroundColor Green
-New-ManagementRoleAssignment -SecurityGroup "Organization Management" -Role "Address Lists"
-Write-Host "Done...." -ForegroundColor Cyan
-Write-Host "After adding role 'Address Lists' to 'Organization Management', reconnecting to Exchange Online" -ForegroundColor Yellow
-
+try {
+    Write-Host "Adding role 'Address Lists' to 'Organization Management' to allow for Address Book Management with the GA Account" -ForegroundColor Green
+    New-ManagementRoleAssignment -SecurityGroup "Organization Management" -Role "Address Lists"
+    Write-Host "Done...." -ForegroundColor Cyan
+    Write-Host "After adding role 'Address Lists' to 'Organization Management', reconnecting to Exchange Online" -ForegroundColor Yellow
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host $ErrorMessage -ForegroundColor red
+}
 
 Write-Host "Waiting 1 minute for the change to take effect." -ForegroundColor Green
 for ($i = 60; $i -ge 0; $i--) {
@@ -91,23 +96,42 @@ Write-Host "Done...." -ForegroundColor Cyan
 Write-Host "Signing back into Exchange Online to reflect new roles" -ForegroundColor Green
 Connect-ExchangeOnline
 
-Write-Host "Creating Address Book Policy" -ForegroundColor Green
-#use the current OAB
-$oab = Get-OfflineAddressBook 'Default Offline Address Book'
-#use the current GAL
-$gal = Get-GlobalAddressList 'Default Global Address List'
+try {
+    Write-Host "Creating Address Book Policy" -ForegroundColor Green
+    #use the current OAB
+    $oab = Get-OfflineAddressBook 'Default Offline Address Book'
+    #use the current GAL
+    $gal = Get-GlobalAddressList 'Default Global Address List'
 
-#Create new Address Book Policy using the same parameters of the default OOTB policy.
-New-AddressBookPolicy -Name "Contoso Address Book" -AddressLists "\Offline Global Address List", "\All Contacts", "\All Distribution Lists", "\All Rooms", "\All Users", "\All Groups", "\Public Folders" -OfflineAddressBook $oab -GlobalAddressList $gal -RoomList "\All Rooms"
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host $ErrorMessage -ForegroundColor red
+}
 
-Write-Host "Done...." -ForegroundColor Cyan
+try {
+    #Create new Address Book Policy using the same parameters of the default OOTB policy.
+    New-AddressBookPolicy -Name "Contoso Address Book" -AddressLists "\Offline Global Address List", "\All Contacts", "\All Distribution Lists", "\All Rooms", "\All Users", "\All Groups", "\Public Folders" -OfflineAddressBook $oab -GlobalAddressList $gal -RoomList "\All Rooms"
 
-#Assigns the new Address Book Policy to all mailboxes
-Write-Host "Assiging all Mailboxes to new Address Book policy" -ForegroundColor Green
+    Write-Host "Done...." -ForegroundColor Cyan
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host $ErrorMessage -ForegroundColor red
+}
 
-get-Mailbox | Set-Mailbox -AddressBookPolicy "Contoso Address Book" 
+try {
+    #Assigns the new Address Book Policy to all mailboxes
+    Write-Host "Assiging all Mailboxes to new Address Book policy" -ForegroundColor Green
 
-Write-Host "Done...." -ForegroundColor Cyan
+    get-Mailbox | Set-Mailbox -AddressBookPolicy "Contoso Address Book" 
+
+    Write-Host "Done...." -ForegroundColor Cyan
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host $ErrorMessage -ForegroundColor red
+}
 
 
 ##############
@@ -127,87 +151,127 @@ Write-Host "Done...." -ForegroundColor Cyan
 #Start-Process "https://login.microsoftonline.com/common/adminconsent?client_id=$appId"
 
 #Enabling AuditLog for your Tenant
-Write-Host "Enabling AuditLog for your Tenant" -ForegroundColor Green
+try {
+    Write-Host "Enabling AuditLog for your Tenant" -ForegroundColor Green
 
-Enable-OrganizationCustomization
-Set-AdminAuditLogConfig -UnifiedAuditLogIngestionEnabled $true
+    Enable-OrganizationCustomization
+    Set-AdminAuditLogConfig -UnifiedAuditLogIngestionEnabled $true
 
-Write-Host "Done..." -ForegroundColor Cyan
-
-# Apply departments to users
-$departments = @('HR', 'Sales', 'Research')
-
-Write-Host "Applying Departments for all users" -ForegroundColor Green
-$users = get-user | where { $_.SKUAssigned -eq $true }
-
-for ($i = 0; $i -lt $users.Count; $i++) {
-    $user = $users[$i]
-    $department = $departments[$i % $departments.Count]
-
-    # Example: Update user department in Active Directory
-    Set-User -Identity $user -Department $department -Confirm:$false
-
-    Write-Host "Updated $user with department $department"
+    Write-Host "Done..." -ForegroundColor Cyan
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host $ErrorMessage -ForegroundColor red
 }
 
-Write-Host "Done..." -ForegroundColor Cyan
+try {
+    # Apply departments to users
+    $departments = @('HR', 'Sales', 'Research')
 
+    Write-Host "Applying Departments for all users" -ForegroundColor Green
+    $users = get-user | where { $_.SKUAssigned -eq $true }
 
-#Create Segments based on department
-Write-Host "Creating IB Segments" -ForegroundColor Green
+    for ($i = 0; $i -lt $users.Count; $i++) {
+        $user = $users[$i]
+        $department = $departments[$i % $departments.Count]
 
-New-OrganizationSegment -Name "HR" -UserGroupFilter "Department -eq 'HR'"
-New-OrganizationSegment -Name "Sales" -UserGroupFilter "Department -eq 'Sales'"
-New-OrganizationSegment -Name "Research" -UserGroupFilter "Department -eq 'Research'"
+        # Example: Update user department in Active Directory
+        Set-User -Identity $user -Department $department -Confirm:$false
 
-Write-Host "Done..." -ForegroundColor Cyan
+        Write-Host "Updated $user with department $department"
+    }
 
+    Write-Host "Done..." -ForegroundColor Cyan
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host $ErrorMessage -ForegroundColor red
+}
 
+try {
+    #Create Segments based on department
+    Write-Host "Creating IB Segments" -ForegroundColor Green
 
-#Create InformationBarrier Policies
-Write-Host "Creating IB Policies" -ForegroundColor Green
+    New-OrganizationSegment -Name "HR" -UserGroupFilter "Department -eq 'HR'"
+    New-OrganizationSegment -Name "Sales" -UserGroupFilter "Department -eq 'Sales'"
+    New-OrganizationSegment -Name "Research" -UserGroupFilter "Department -eq 'Research'"
 
-if ($policytype -eq 'Block') {
-    #Blocks
-    New-InformationBarrierPolicy -Name "Sales - Blocks - Research" -AssignedSegment "Sales" -SegmentsBlocked "Research" -State  "active"
-    New-InformationBarrierPolicy -Name "Research - Blocks - Sales" -AssignedSegment "Research" -SegmentsBlocked "Sales" -State "active"
+    Write-Host "Done..." -ForegroundColor Cyan
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host $ErrorMessage -ForegroundColor red
 }
 
 
-if ($policytype -eq 'Allow') {
-    New-InformationBarrierPolicy -Name "HR - Allows - Research and Sales" -AssignedSegment "HR" -SegmentsAllowed "HR", "Research", "Sales" -State "active"
-    New-InformationBarrierPolicy -Name "Sales - Allows - HR" -AssignedSegment "Sales" -SegmentsAllowed "Sales", "HR" -State "active"
-    New-InformationBarrierPolicy -Name "Research Allows - HR" -AssignedSegment "Research" -SegmentsAllowed "Research", "HR" -State "active"
+try {
+    #Create InformationBarrier Policies
+    Write-Host "Creating IB Policies" -ForegroundColor Green
+
+    if ($policytype -eq 'Block') {
+        #Blocks
+        New-InformationBarrierPolicy -Name "Sales - Blocks - Research" -AssignedSegment "Sales" -SegmentsBlocked "Research" -State  "active"
+        New-InformationBarrierPolicy -Name "Research - Blocks - Sales" -AssignedSegment "Research" -SegmentsBlocked "Sales" -State "active"
+    }
+
+
+    if ($policytype -eq 'Allow') {
+        New-InformationBarrierPolicy -Name "HR - Allows - Research and Sales" -AssignedSegment "HR" -SegmentsAllowed "HR", "Research", "Sales" -State "active"
+        New-InformationBarrierPolicy -Name "Sales - Allows - HR" -AssignedSegment "Sales" -SegmentsAllowed "Sales", "HR" -State "active"
+        New-InformationBarrierPolicy -Name "Research Allows - HR" -AssignedSegment "Research" -SegmentsAllowed "Research", "HR" -State "active"
+    }
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host $ErrorMessage -ForegroundColor red
 }
 
 
-#Start Policy Application
-Write-Host "Starting  Information Barrier Policies Application" -ForegroundColor Green
-Start-InformationBarrierPoliciesApplication -Confirm:$false #IPPSSession Needed
-Write-Host "The job has been created but will take about 1 hour to complete." -ForegroundColor Cyan
+try {
+    #Start Policy Application
+    Write-Host "Starting  Information Barrier Policies Application" -ForegroundColor Green
+    Start-InformationBarrierPoliciesApplication -Confirm:$false #IPPSSession Needed
+    Write-Host "The job has been created but will take about 1 hour to complete." -ForegroundColor Cyan
 
-Write-Host "" -ForegroundColor Cyan
+    Write-Host "" -ForegroundColor Cyan
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host $ErrorMessage -ForegroundColor red
+}
 
-Write-Host "Gettinng Information Barrier Policies Application Status" -ForegroundColor Green
-Get-InformationBarrierPoliciesApplicationStatus  #IPPSSession Needed
-Write-Host "Done..." -ForegroundColor Cyan
+try {
+    #Get Policy Application
+    Write-Host "Gettinng Information Barrier Policies Application Status" -ForegroundColor Green
+    Get-InformationBarrierPoliciesApplicationStatus  #IPPSSession Needed
+    Write-Host "Done..." -ForegroundColor Cyan
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host $ErrorMessage -ForegroundColor red
+}
 
-#Enable IB for SharePoint
-Write-Host "Enabling IB for SharePoint" -ForegroundColor Green
+try {
+    #Enable IB for SharePoint
+    Write-Host "Enabling IB for SharePoint" -ForegroundColor Green
 
+    #To enable information barriers in SharePoint and OneDrive
+    Set-SPOTenant -InformationBarriersSuspension $false
 
-#To enable information barriers in SharePoint and OneDrive
-Set-SPOTenant -InformationBarriersSuspension $false
+    #Enable Group Discoverability in SPO
+    Set-SPOTenant -ShowPeoplePickerGroupSuggestionsForIB $true
 
-#Enable Group Discoverability in SPO
-Set-SPOTenant -ShowPeoplePickerGroupSuggestionsForIB $true
+    #needed for Teams Recordings
+    Set-SPOTenant -AppOnlyBypassPeoplePickerPolicies $true
+    Set-SPOTenant -AppBypassInformationBarriers $true
 
-#needed for Teams Recordings
-Set-SPOTenant -AppOnlyBypassPeoplePickerPolicies $true
-Set-SPOTenant -AppBypassInformationBarriers $true
-
-#enable for Teams (IBV1 Setting)
-Set-SPOTenant -IBImplicitGroupBased $true
+    #enable for Teams (IBV1 Setting)
+    Set-SPOTenant -IBImplicitGroupBased $true
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host $ErrorMessage -ForegroundColor red
+}
 
 #optional
 
@@ -220,91 +284,147 @@ Set-SPOTenant -IBImplicitGroupBased $true
 
 Write-Host "Done..." -ForegroundColor Cyan
 
+try {
+    #udpate OneDrive sites:
+    Write-Host "Stamping exsiting Onedrive sites with Segments" -ForegroundColor Green
+    $updateODB = Start-SPOInformationBarriersPolicyComplianceReport -UpdateOneDriveSegments -Confirm:$false
+    Write-Host "Process started but will take about 1 hour to compelte" -ForegroundColor Cyan
+    Write-Host ""
 
-#udpate OneDrive sites:
-Write-Host "Stamping exsiting Onedrive sites with Segments" -ForegroundColor Green
-$updateODB = Start-SPOInformationBarriersPolicyComplianceReport -UpdateOneDriveSegments -Confirm:$false
-Write-Host "Process started but will take about 1 hour to compelte" -ForegroundColor Cyan
-Write-Host ""
+    Write-Host "Information Barriers has been configued but will take up to 24 hours to be fully functional!!" -ForegroundColor Green
+    Write-Host ""
 
-Write-Host "Information Barriers has been configued but will take up to 24 hours to be fully functional!!" -ForegroundColor Green
-Write-Host ""
-
-Write-Host "Checking current state of Information Barriers" -ForegroundColor Cyan
-
-#Get Segments:
-Write-Host "Getting IB Segments" -ForegroundColor Green
-Get-OrganizationSegment | fl  name, UserGroupFilter, ExoSegmentId #IPPSSession Needed
-
-Write-Host ""
-
-#Get  IB Policies
-Write-Host "Getting IB Policies" -ForegroundColor Green
-Get-InformationBarrierPolicy | FL  Name, AssignedSegment, SegmentsBlocked, SegmentsAllowed, ExoPolicyId, State, Guid , SegmentsAllowed, BlockVisibility, SegmentsBlocked, state #IPPSSession Needed
-
-Write-Host ""
-
-#Get Org Level Settings
-Write-Host "Getting IB Org level settings" -ForegroundColor Green
-Get-OrganizationConfig |  fl *IB*, *info* #ExchangeOnline Needed
-Get-PolicyConfig |  fl *IB*, *info* #ExchangeOnline Needed
-
-Write-Host ""
-
-#Check SPO Settings
-Write-Host "Getting IB settings in SPO" -ForegroundColor Green
-Get-Spotenant | fl DefaultOneDriveInformationBarrierMode, InformationBarriersSuspension, IBImplicitGroupBased, ShowPeoplePickerGroupSuggestionsForIB, *bypass* #SPOService Needed
-
-Write-Host ""
-
-#check if users are compatible with each other:
-Write-Host "Checking random user IB compatibility" -ForegroundColor Green
-$users = get-user | where { $_.SKUAssigned -eq $true } | select UserPrincipalName
-$randomUsers = $users | Get-Random
-
-foreach ($randomUser in $randomUsers) {
-    $randomUser2 = $users | Get-Random
-    $results = Get-ExoInformationBarrierRelationship -RecipientId1 $randomUser.UserPrincipalName -RecipientId2 $randomUser2.UserPrincipalName
-    $r1 = $results | select RecipientName1
-    $r2 = $results | select RecipientName2
-    $RecipientName1 = get-user $r1.RecipientName1 | select UserPrincipalName
-    $RecipientName2 = get-user $r2.RecipientName2 | select UserPrincipalName
-    $results 
-    write-host "RecipientName1 is" $RecipientName1.UserPrincipalName
-    write-host "RecipientName2 is" $RecipientName2.UserPrincipalName
-    $randomUser = @()
-    $randomUser2 = @() 
+    Write-Host "Checking current state of Information Barriers" -ForegroundColor Cyan
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host $ErrorMessage -ForegroundColor red
 }
 
-Write-Host ""
+try {
+    #Get Segments:
+    Write-Host "Getting IB Segments" -ForegroundColor Green
+    Get-OrganizationSegment | fl  name, UserGroupFilter, ExoSegmentId #IPPSSession Needed
 
-#get IB Settings per user
-Write-Host "Getting IB per user" -ForegroundColor Green
-$users = get-user | where { $_.SKUAssigned -eq $true }
-foreach ($user in $users) { 
-    Get-Recipient -Identity $user.UserPrincipalName | fl DisplayName, name, InformationBarrierSegments, WhenIBSegmentChanged, Department, AddressBookPolicy
+    Write-Host ""
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host $ErrorMessage -ForegroundColor red
 }
 
-Write-Host ""
+try {
+    #Get  IB Policies
+    Write-Host "Getting IB Policies" -ForegroundColor Green
+    Get-InformationBarrierPolicy | FL  Name, AssignedSegment, SegmentsBlocked, SegmentsAllowed, ExoPolicyId, State, Guid , SegmentsAllowed, BlockVisibility, SegmentsBlocked, state #IPPSSession Needed
 
-
-#get IB Settings per ODB Site 
-Write-Host "Getting IB Settings in OneDrive Sites" -ForegroundColor Green
-$odburls = Get-SPOSite -IncludePersonalSite $true -Limit all -Filter "Url -like '-my.sharepoint.com/personal/'" | Select -ExpandProperty Url
-foreach ($odburl in $odburls) { 
-    get-sposite -Identity $odburl | FL Owner, URL, InformationSegment, InformationBarriersMode
+    Write-Host ""
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host $ErrorMessage -ForegroundColor red
 }
 
-Write-Host ""
 
-#get IB Settings SPO Site
-Write-Host "Getting IB Settings in OneDrive Sites" -ForegroundColor Green
-$sposites = Get-SPOSite -IncludePersonalSite $false -Limit all | Select -ExpandProperty Url
-foreach ($sposite in $sposites) { 
-    get-sposite -Identity $sposite | FL Owner, URL, InformationSegment, InformationBarriersMode
+try {
+    #Get Org Level Settings
+    Write-Host "Getting IB Org level settings" -ForegroundColor Green
+    Get-OrganizationConfig |  fl *IB*, *info* #ExchangeOnline Needed
+    Get-PolicyConfig |  fl *IB*, *info* #ExchangeOnline Needed
+
+    Write-Host ""
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host $ErrorMessage -ForegroundColor red
 }
 
-Write-Host ""
+try {
+    #Check SPO Settings
+    Write-Host "Getting IB settings in SPO" -ForegroundColor Green
+    Get-Spotenant | fl DefaultOneDriveInformationBarrierMode, InformationBarriersSuspension, IBImplicitGroupBased, ShowPeoplePickerGroupSuggestionsForIB, *bypass* #SPOService Needed
+
+    Write-Host ""
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host $ErrorMessage -ForegroundColor red
+}
+
+try {
+    #check if users are compatible with each other:
+    Write-Host "Checking random user IB compatibility" -ForegroundColor Green
+    $users = get-user | where { $_.SKUAssigned -eq $true } | select UserPrincipalName
+    $randomUsers = $users | Get-Random
+
+    foreach ($randomUser in $randomUsers) {
+        $randomUser2 = $users | Get-Random
+        $results = Get-ExoInformationBarrierRelationship -RecipientId1 $randomUser.UserPrincipalName -RecipientId2 $randomUser2.UserPrincipalName
+        $r1 = $results | select RecipientName1
+        $r2 = $results | select RecipientName2
+        $RecipientName1 = get-user $r1.RecipientName1 | select UserPrincipalName
+        $RecipientName2 = get-user $r2.RecipientName2 | select UserPrincipalName
+        $results 
+        write-host "RecipientName1 is" $RecipientName1.UserPrincipalName
+        write-host "RecipientName2 is" $RecipientName2.UserPrincipalName
+        $randomUser = @()
+        $randomUser2 = @() 
+    }
+
+    Write-Host ""
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host $ErrorMessage -ForegroundColor red
+}
+
+
+try {
+    #get IB Settings per user
+    Write-Host "Getting IB per user" -ForegroundColor Green
+    $users = get-user | where { $_.SKUAssigned -eq $true }
+    foreach ($user in $users) { 
+        Get-Recipient -Identity $user.UserPrincipalName | fl DisplayName, name, InformationBarrierSegments, WhenIBSegmentChanged, Department, AddressBookPolicy
+    }
+
+    Write-Host ""
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host $ErrorMessage -ForegroundColor red
+}
+
+
+try {
+    #get IB Settings per ODB Site 
+    Write-Host "Getting IB Settings in OneDrive Sites" -ForegroundColor Green
+    $odburls = Get-SPOSite -IncludePersonalSite $true -Limit all -Filter "Url -like '-my.sharepoint.com/personal/'" | Select -ExpandProperty Url
+    foreach ($odburl in $odburls) { 
+        get-sposite -Identity $odburl | FL Owner, URL, InformationSegment, InformationBarriersMode
+    }
+
+    Write-Host ""
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host $ErrorMessage -ForegroundColor red
+}
+
+try {
+    #get IB Settings SPO Site
+    Write-Host "Getting IB Settings in OneDrive Sites" -ForegroundColor Green
+    $sposites = Get-SPOSite -IncludePersonalSite $false -Limit all | Select -ExpandProperty Url
+    foreach ($sposite in $sposites) { 
+        get-sposite -Identity $sposite | FL Owner, URL, InformationSegment, InformationBarriersMode
+    }
+
+    Write-Host "" 
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host $ErrorMessage -ForegroundColor red
+}
+
 
 Write-Host "Done........." -ForegroundColor Cyan
 Write-Host "Information Barriers have been setup for $T, it could take 24 hours to take full effect." -ForegroundColor Green
