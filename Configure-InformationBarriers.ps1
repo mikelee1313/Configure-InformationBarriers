eb69c4a335a617ed8c2bec3d72fe6b84dfcf81c1
Mike Lee <62190454+mikelee1313@users.noreply.github.com>
2024-10-07 16:52:50 -0400

    Update Configure-InformationBarriers.ps1

--- Configure-InformationBarriers.ps1
+++ Configure-InformationBarriers.ps1
@@ -3,21 +3,24 @@
 This script configures Information Barriers (IB) and Address Book Policies in an Office 365 tenant.
 
 .DESCRIPTION
+
 The script performs the following tasks:
+
 1. Prompts the user for the tenant name and the type of policies (Allow or Block).
 2. Connects to various Office 365 services including Exchange Online, SharePoint Online, and IPPS.
-3. Creates an Address Book Policy to prevent an empty address book.
-4. Assigns the new Address Book Policy to all mailboxes.
-5. Applies department attributes to users.
-6. Creates organization segments based on departments.
-7. Creates Information Barrier Policies based on the selected policy type (Allow or Block).
-8. Starts the application of Information Barrier Policies.
-9. Enables Information Barriers for SharePoint and OneDrive.
-10. Triggers Personal Site creations using"Request-SPOPersonalSite" for all users.
-11. Updates existing OneDrive sites with segments.
-12. Checks the current state of Information Barriers and retrieves various IB settings.
-13. Checks IB compatibility between random users.
-14. Retrieves IB settings for users, OneDrive sites, and SharePoint sites.
+3. Enables Organization Customizations
+4. Creates an Address Book Policy to prevent an empty address book.
+5. Assigns the new Address Book Policy to all mailboxes.
+6. Applies department attributes to users.
+7. Creates organization segments based on departments.
+8. Creates Information Barrier Policies based on the selected policy type (Allow or Block).
+9. Starts the application of Information Barrier Policies.
+10. Enables Information Barriers for SharePoint and OneDrive.
+11. Triggers Personal Site creations using "Request-SPOPersonalSite" for all users.
+12. Updates existing OneDrive sites with segments.
+13. Checks the current state of Information Barriers and retrieves various IB settings.
+14. Checks IB compatibility between random users.
+15. Retrieves IB settings for users, OneDrive sites, and SharePoint sites.
 
 .PARAMETER TenantName
 The name of the Office 365 tenant (e.g., M365x03708457).
